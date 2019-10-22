5598c52606265b010bf9bfefafde1b34bbb676e4
Andrzej <mucha.andrzej94@gmail.com>
2019-10-22 10:27:10 +0200

    Użycie usługi w komponencie nav.

--- PortalRandowy.SPA/src/app/nav/nav.component.ts
+++ PortalRandowy.SPA/src/app/nav/nav.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../_services/auth.service';
+
 
 @Component({
   selector: 'app-nav',
@@ -8,12 +10,16 @@ import { Component, OnInit } from '@angular/core';
 export class NavComponent implements OnInit {
 
   model: any = {};
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
   }
 
-  login(){
-    console.log(this.model);
+  login() {
+    this.authService.login(this.model).subscribe(next => {
+      console.log('Zalogowałeś się do aplikacji');
+    }, error => {
+      console.log('Wystąpił błąd logowania');
+    });
   }
 }
