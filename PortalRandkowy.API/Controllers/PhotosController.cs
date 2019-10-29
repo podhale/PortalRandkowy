581ed7274f8a69c6f2640ca2c0c10751671c6f16
Andrzej <mucha.andrzej94@gmail.com>
2019-10-29 19:54:28 +0100

    Kontroler zdjęć

---
+++ PortalRandkowy.API/Controllers/PhotosController.cs
@@ -0,0 +1,80 @@
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using AutoMapper;
+using CloudinaryDotNet;
+using CloudinaryDotNet.Actions;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using PortalRandkowy.API.Data;
+using PortalRandkowy.API.Dtos;
+using PortalRandkowy.API.Helpers;
+using PortalRandkowy.API.Models;
+
+namespace PortalRandkowy.API.Controllers
+{
+    [Authorize]
+    [Route("api/users/{userId}/photos")]
+    public class PhotosController : ControllerBase
+    {
+        private readonly IUserRepository _repository;
+        private readonly IMapper _mapper;
+        private readonly IOptions<CloudinarySettings> _cloudinarySettings;
+        private Cloudinary _cloudinary;
+
+        public PhotosController(IUserRepository repository, IMapper mapper, IOptions<CloudinarySettings> cloudinarySettings)
+        {
+            _cloudinarySettings = cloudinarySettings;
+            _mapper = mapper;
+            _repository = repository;
+
+            Account account = new Account (
+                _cloudinarySettings.Value.CloudName,
+                _cloudinarySettings.Value.ApiKey,
+                _cloudinarySettings.Value.ApiSecret
+            );
+
+            _cloudinary = new Cloudinary(account);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddPhotoForUser(int userId, PhotoForCreationDto photoForCreationDto) {
+            
+            if (userId != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
+                return Unauthorized();
+
+            var userFromRepo = await _repository.GetUser(userId);
+            var file = photoForCreationDto.File;
+            var uploadResult = new ImageUploadResult();
+
+            if(file.Length > 0)
+            {
+                using ( var stram = file.OpenReadStream())
+                {
+                    var uploadParams = new ImageUploadParams() {
+                        File = new FileDescription(file.Name, stram),
+                        Transformation = new Transformation().Width(500).Height(500).Crop("fill").Gravity("face")
+                    };
+                    uploadResult = _cloudinary.Upload(uploadParams);
+                }
+                
+            }
+
+            photoForCreationDto.Url = uploadResult.Uri.ToString();
+            photoForCreationDto.PublicId = uploadResult.PublicId;
+
+            var photo = _mapper.Map<Photo>(photoForCreationDto);
+
+            if (!userFromRepo.Photos.Any(p => p.IsMain ))
+                photo.IsMain = true;
+
+            userFromRepo.Photos.Add(photo);
+
+            if (await _repository.saveAll())
+                return Ok();
+
+            return BadRequest("Nie można dodać zdjęcia");
+        }
+    }
+}
