c3facab22b4bc5e810d6c8591f54e90df0969d95
Andrzej <mucha.andrzej94@gmail.com>
2019-11-04 12:15:04 +0100

    Poprawiki do UserParams

--- PortalRandkowy.API/Helpers/UserParams.cs
+++ PortalRandkowy.API/Helpers/UserParams.cs
@@ -7,8 +7,8 @@ namespace PortalRandkowy.API.Helpers
         public const int MaxPageSize = 48;
 
         public int PageSize {
-            get { return pageSize };
-            set { pageSize = (value > MaxPageSize) ? MaxPageSize : value}
+            get { return pageSize; }
+            set { pageSize = (value > MaxPageSize) ? MaxPageSize : value; }
         }
     }
 }
